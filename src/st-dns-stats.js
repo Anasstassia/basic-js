afb1e43047733e9584a136bc14c01e8d723e355e
Anasstassia <gubinaa75@gmail.com>
2021-09-29 17:11:04 +0300

    done

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,7 +22,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  const obj = {};
+  const arr = domains.map((domain) => domain.split("."));
+  arr.map((el) => {
+    let property = `.${el[el.length - 1]}`;
+    for (let i = 1; i < el.length + 1; i++) {
+      if (obj.hasOwnProperty(property)) {
+        obj[property] += 1;
+      } else {
+        obj[property] = 1;
+      }
+      property += `.${el[el.length - 1 - i]}`;
+      //   console.log(property);
+    }
+  });
+  return obj;
 }
